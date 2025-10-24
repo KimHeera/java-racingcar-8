ce98e3bd2a0b83c92440212eec0705dddf9e8147
kimheera <hrk0295@naver.com>
2025-10-24 16:18:30 +0900

    feat: 입력된 문자열 유효성 검증 기능 구현
    입력된 문자열이 빈 값인지, 공백이 포함되어 있는지, 5자를 초과하는지의 유효성을 검증한다.
    
    원래 있던 쉼표 구분 기능은 추후 다른 위치로 옮길 예정

--- src/main/java/racingcar/domain/Validator.java
+++ src/main/java/racingcar/domain/Validator.java
@@ -1,15 +1,32 @@
 package racingcar.domain;
 
-public class Validator {
-    public void validateName(String inputCarName){
-        String[] carNames;
+import java.util.List;
 
-        if(inputCarName.contains(",")){
-            carNames = splitName(inputCarName);
+public class Validator {
+    final int MAX_NAME_LENGTH =5;
+
+    public void validateNames(String inputCarName){
+        inputCarName = inputCarName.trim();
+
+        String[] splitName = inputCarName.split(",");
+        List<String> carNames = List.of(splitName);
+
+        if(carNames.isEmpty() && inputCarName.isEmpty()){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름 목록이 비어 있습니다.");
+        }
+
+        for(String name : carNames){
+            validateSingleName(name.trim());
         }
     }
 
-    public String[] splitName(String inputCarName){
-        return inputCarName.split(",");
+    public void validateSingleName(String name){
+        if(name.isEmpty()){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 빈 값일 수 없습니다.");
+        }
+
+        if(name.length() > MAX_NAME_LENGTH){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름의 길이는 5자 이하만 가능합니다.");
+        }
     }
 }
