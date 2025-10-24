3699baa9c360eb638d98a6c1d54f5939bdc31eff
kimheera <hrk0295@naver.com>
2025-10-25 00:01:08 +0900

    feat: 문자열 리스트를 Car 리스트로 변경하는 기능 구현

---
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+    //List<String> -> List<Car>
+    public static List<Car> createCars(List<String> carNames){
+        List<Car> cars = new ArrayList<>();
+
+        for(String name : carNames){
+            cars.add(new Car(name));
+        }
+
+        return cars;
+    }
+}
