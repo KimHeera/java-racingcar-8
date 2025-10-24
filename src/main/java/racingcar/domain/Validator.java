ba90c59d605c1ba8d42a6be6223f64282a4c9928
kimheera <hrk0295@naver.com>
2025-10-24 15:24:08 +0900

    feat: 쉼표 분리 기능 구현
    입력된 문자열에 쉼표가 포함되어 있다면, 쉼표를 기준으로 이름을 분리하는 기능을 구현했다.

--- src/main/java/racingcar/domain/Validator.java
+++ src/main/java/racingcar/domain/Validator.java
@@ -1,4 +1,15 @@
 package racingcar.domain;
 
 public class Validator {
+    public void validateName(String inputCarName){
+        String[] carNames;
+
+        if(inputCarName.contains(",")){
+            carNames = splitName(inputCarName);
+        }
+    }
+
+    public String[] splitName(String inputCarName){
+        return inputCarName.split(",");
+    }
 }
