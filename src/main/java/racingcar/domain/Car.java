61481a5453cc5cab9f537ddc823c867658ac0d70
kimheera <hrk0295@naver.com>
2025-10-25 00:00:29 +0900

    feat: Car 필드, 이동 조건을 만족한 이동 함수 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,21 @@
 package racingcar.domain;
 
 public class Car {
+    private final String name;
+    private int position; //initial 0
+
+    public Car(String carName){
+        this.name = carName;
+        this.position = 0;
+    }
+
+    public void move(int randNumber){
+        if(isMovable(randNumber)){
+            position++;
+        }
+    }
+
+    private boolean isMovable(int randNumber){
+        return randNumber >= 4;
+    }
 }
