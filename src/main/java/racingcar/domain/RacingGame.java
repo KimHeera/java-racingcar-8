e8afaf1d2664c3ecdf8ffe926eb9cf56d0ae8c7d
kimheera <hrk0295@naver.com>
2025-10-25 23:30:00 +0900

    feat: 0 - 9 사이의 무작위 값을 구한 후, 4 이상이라면 전진 기능 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,32 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class RacingGame {
+    private final List<Car> carList;
+
+    public RacingGame(List<Car> carList){
+        this.carList = carList;
+    }
+
+    //round
+    public void moveRound(){
+        for(Car car : carList){
+            attemptMove(car);
+        }
+    }
+
+    private void attemptMove(Car car){
+        int randNumber = randNum();
+
+        car.move(randNumber);
+    }
+
+    //무작위값
+    private int randNum(){
+        return Randoms.pickNumberInRange(0, 9);
+    }
+
+    //우승자 가리기
 }
