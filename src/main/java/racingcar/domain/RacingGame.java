6758f4539c54afa7e2b42fdc4fa947c9b36c0516
kimheera <hrk0295@naver.com>
2025-10-26 00:52:15 +0900

    feat: 우승자 가리는 기능 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
@@ -33,4 +34,35 @@ public class RacingGame {
     }
 
     //우승자 가리기
+    public List<String> getWinner(){
+        int max = 0;
+
+        for(Car car : carList){
+            max = comparePosition(max, car.getPosition());
+        }
+
+        return winnersList(max);
+    }
+
+    private int comparePosition(int max, int position){
+        if(position > max){
+            int tmp = max;
+            max = position;
+            position = tmp;
+        }
+
+        return max;
+    }
+
+    private List<String> winnersList(int max){
+        List<String> winners = new ArrayList<>();
+
+        for(Car car : carList){
+            if(max == car.getPosition()){
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
+    }
 }
